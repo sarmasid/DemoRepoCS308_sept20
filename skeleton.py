759cb1cbfa5db4fc424d8d900db42c83a16cfbd0
Paddy <paddy@kestrel.iitmandi.ac.in>
2020-10-06 12:04:57 +0530

    modified:   skeleton.py 	Added skeleton for Some and Avantika to fill in

--- skeleton.py
+++ skeleton.py
@@ -1,4 +1,23 @@
 # this is the skeleton python code.
 
-print('Hello world!')
+
+# Soma
+#def getFirstWord():
+	# Write the code here
+
+
+
+# Avantika
+#def getLatWord():
+	# Write the code here
+
+
+
+str = 'The Good The Bad and The Ugly'
+print(str)
+print(getFirstWord(str))
+print(getLastWord(str))
+
+
+
 
